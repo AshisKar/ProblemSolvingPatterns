42c6e3f794ec0e4c5ea1900a89ae95a57488b5ca
Ashis Kar <karashis064@gmail.com>
2021-04-22 09:10:33 +0530

    Create fibonacci.py

---
+++ dynamicProgramming/fibonacci.py
@@ -0,0 +1,14 @@
+memo = {} #dictionay for Memoization
+
+def fib(n):
+  if n == 0: # base case 1
+    return 0
+  if n == 1: # base case 2
+    return 1
+  elif n in memo: # Check if result for n has already been evaluated
+    return memo[n] # return the result if it is available
+  else: # otherwise recursive step
+    memo[n] = fib(n-1) + fib(n-2) # store the result of n in memoization dictionary
+    return memo[n] # return the value
+
+print (fib(100))
